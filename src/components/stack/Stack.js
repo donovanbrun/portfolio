8dae5a793baea95da5f39480e585946d2fec6717
donovanbrun <donovanbrun@gmail.com>
2022-09-13 01:00:45 +0200

    some changes

--- src/components/stack/Stack.js
+++ src/components/stack/Stack.js
@@ -5,24 +5,10 @@ const Stack = () => {
 
     return (
         <div className="Stack container" id="Stack">
-            <h1 className="Stack-Title">My Stack</h1>
-            <div className="Stacks">
-                <div className="Stack-Item">
-                    <h2 className="Stack-Item-Title">Backend</h2>
-                    <ul className="Stack-Item-List">
-                        <li className="Stack-Item-List-Item">
-                            <span className="Stack-Item-List-Item-Title">React</span>
-                        </li>
-                    </ul>
-                </div>
-                <div className="Stack-Item">
-                    <h2 className="Stack-Item-Title">Frontend</h2>
-                    <ul className="Stack-Item-List">
-                        <li className="Stack-Item-List-Item">
-                            <span className="Stack-Item-List-Item-Title">React</span>
-                        </li>
-                    </ul>
-                </div>
+            <h1 className="StackTitle">What can I do?</h1>
+            <div className='Stacks'>
+                <p className='StacksText'>Build fullstack application</p>
+                <p className='StacksText'>From the conception to the deployment</p>
             </div>
         </div>
     );
