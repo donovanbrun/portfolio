51c0e0377a0596327bac8474c4c20b12bec515aa
donovanbrun <donovanbrun@gmail.com>
2023-07-23 17:55:56 +0200

    fix font and perf

--- js/main.js
+++ js/main.js
@@ -122,7 +122,7 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const STAR_COUNT = 150
+const STAR_COUNT = 100
 let result = ""
 for(let i = 0; i < STAR_COUNT; i++){
     result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
